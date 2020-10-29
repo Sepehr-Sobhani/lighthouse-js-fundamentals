b2730f100bd3bb07e0951f9201aa46e418e85fe1
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-10-29 22:57:06 +0000

    which school challenege by Lighthouse Labs compass

---
+++ which-school.js
@@ -0,0 +1,9 @@
+// Elementary School if age is below 13
+// Secondary School if age is between 13 and 18 (both inclusive)
+// Lighthouse Labs in all other cases.
+
+const whichSchool = function (age) {
+  if (age < 13) return "Elementary School";
+  else if (age >= 13 && age <= 18) return "Secondary School";
+  return "Lighthouse Labs";
+};
